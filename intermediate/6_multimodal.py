572098154e4dded119f34d35cd19314be6ed0176
FredoVz <dm.fredo018@gmail.com>
2026-07-20 22:38:35 +0700

    Belajar image understanding & video generation

--- intermediate/6_multimodal.py
+++ intermediate/6_multimodal.py
@@ -1,5 +1,7 @@
 import os
+import time
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 from pathlib import Path
@@ -30,8 +32,44 @@ def image_generation():
 
     print("Image successfully generated!")
 
+def image_understanding():
+    with open("multimodal/sample-image.jpg", "rb") as f:
+        image_bytes = f.read()
+
+    prompt = input("Prompt image understanding: \n")
+
+    response = client.models.generate_content(
+        model=os.getenv("GEMINI_MODEL"), 
+        contents=[
+            prompt, 
+            types.Part.from_bytes(data=image_bytes, mime_type="image/jpeg")
+        ], # Paid API
+    )
+
+    print(response.text)
+
+def video_generation():
+    prompt = input("Prompt video generation: \n")
+
+    operation = client.models.generate_videos(
+        model=os.getenv("GEMINI_MODEL_VIDEO"), prompt=prompt, # Paid API
+    )
+
+    while not operation.done:
+        print("Menunggu proses video generation selesai ... ")
+        time.sleep(10)
+        operation = client.operations.get(operation)
+
+    generated_video = operation.response.generated_videos[0]
+    client.files.download(file=generated_video)
+
+    generated_video.video.save("multimodal/generated-video.mp4")
+    print("Video berhasil di simpan ke multimodal/generated-video.mp4")
+
 def main():
-    image_generation()
+    # image_generation()
+    # image_understanding()
+    video_generation()
 
 main()
 
