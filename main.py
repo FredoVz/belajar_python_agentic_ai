81d6a693fd473378aa84a619c8caf03df350a928
FredoVz <dm.fredo018@gmail.com>
2026-07-22 10:32:53 +0700

    feat: add file pertanyaan_jawaban.txt

--- main.py
+++ main.py
@@ -7,14 +7,6 @@ print(year)
 print(in_progress)
 print(f"Selamat datang di kelas {title}")
 
-# === Menjawab Quiz Level Basic ===
-# 1. Perintah apa yang dipakai untuk memastikan Python sudah terinstall?
-# Jawaban : python --version atau python3 --version
-# 2. Khusus pengguna windows, kenapa opsi "Add Python to PATH" penting untuk dicentang?
-# Jawaban : Agar python bisa dijalankan melalui terminal
-# 3. Apa yang terjadi saat masuk ke dalam folder, kemudian menjalankan perintah `uv init .` ?
-# Jawaban : Muncul keterangan initialize di project status pada terminal dan daftar yang dikelola uv.
-
 # Terminal untuk uv
 # uv init .
 # uv init belajar_python_agentic_ai
